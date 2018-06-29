abe8620ae0fa2af66ff96352020e217b65d7e5db
Thien Ho <thienngho@gmail.com>
2018-06-29 13:58:44 +0700

    Use forearch

--- country.js
+++ country.js
@@ -12,16 +12,16 @@ class Country extends EventEmitter {
         try {
             let text = fs.readFileSync(this.filename);
             let jsonContent = JSON.parse(text);
-            let i = 0;
-            for (i; i<jsonContent.length; i++) {
+
+            jsonContent.forEach(item => {
                 var data = {
-                    country: jsonContent[i].country,
-                    city: jsonContent[i].city,
-                    index: i
-                };
+                    country: item.country,
+                    city: item.city,
+                    index: jsonContent.indexOf(item)
+                }
                 this.emit('countrydata', data);
                 sleep.for(1000);
-            }
+            });
         } catch (error) {
             console.error('There was an error: ' +error);
             return;
