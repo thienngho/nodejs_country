0705febca431e98bff8277b3a21dce64d7862a40
Thien Ho <thienngho@gmail.com>
2018-06-29 11:38:11 +0700

    Update code

---
+++ country.js
@@ -0,0 +1,27 @@
+const EventEmitter = require('events');
+const fs = require('fs');
+const sleep = require('sleeping');
+
+class Country extends EventEmitter {
+    constructor(filename) {
+        super();
+        this.filename = filename;
+    }
+
+    readData() {
+        let text = fs.readFileSync(this.filename);
+        let jsonContent = JSON.parse(text);
+        let i = 0;
+            for (i; i<jsonContent.length; i++) {
+                var data = {
+                    country: jsonContent[i].country,
+                    city: jsonContent[i].city,
+                    index: i
+                };
+                this.emit('countrydata', data);
+                sleep.for(1000);
+            }
+    }
+}
+
+module.exports = Country;
--- index.js
+++ index.js
@@ -1,44 +1,11 @@
-const EventEmitter = require('events').EventEmitter;
-const emitter = new EventEmitter();
-const fs = require('fs');
+const Country = require('./country');
 
-emitter.on('start_read', function(file_name){
-    console.log("Started Reading file....");
-    fs.readFile(file_name, 'utf8', function (err, data) {
-        if (err) {
-            emitter.emit('error', err);
-        }
-        else {
-            console.log("Done Reading file....");
-            emitter.emit('print_content', data);
-        }
-    });
+const country = new Country('./country-by-capital-city.json');
 
+country.on('countrydata', function(data){
+    console.log('Country', data.country);
+    console.log('Capital City', data.city);
+    console.log('Country index', data.index);
 });
 
-emitter.on('print_content', function(data){
-    let data_obj = JSON.parse(data);
-
-    data_obj.forEach(function(value) {
-        setTimeout(() => {
-            console.log('Country', value.country);
-            console.log('Capital City', value.city);
-            console.log('Country index', data_obj.indexOf(value));
-        }, 1000);
-    });
-
-});
-
-emitter.on('error',function(type){
-
-    console.log("Faced error: " + type);
-    emitter.emit('done');
-
-});
-
-emitter.emit('start_read','./country-by-capital-city.json');
-
-emitter.removeListener('print_content',function(){
-});
-
-emitter.removeAllListeners('start_read');
+country.readData();
